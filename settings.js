9eff2b124ecdafa8e32fad7ad7fbd4447e91e949
emekavic <emekavic93@gmail.com>
2025-01-04 08:33:17 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 global.bankname = "MONIEPOINT"
 
-global.banknumber = "8133729715"
+global.banknumber = "8106466990"
 
-global.bankowner = "FADARE"
+global.bankowner = "CHUKWUEMEKA"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
